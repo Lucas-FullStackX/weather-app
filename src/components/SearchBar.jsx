7fcd1f13ffebeda39b80704b992a4600257d2443
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2021-08-02 10:57:36 -0500

    update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
+import "./SB.css";
 
-export default function SearchBar({onSearch}) {
+export default function SearchBar({ onSearch }) {
   const [city, setCity] = useState("");
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      onSearch(city);
-    }}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSearch(city);
+      }}
+    >
       <input
         type="text"
-        placeholder="Ciudad..."
+        placeholder="City..."
         value={city}
-        onChange={e => setCity(e.target.value)}
+        className="bars"
+        onChange={(e) => setCity(e.target.value)}
       />
-      <input type="submit" value="Agregar" />
+      <input type="submit" className="lens" value="Search" />
     </form>
   );
 }
